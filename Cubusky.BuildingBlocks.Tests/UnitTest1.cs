ec8627403d054c63917cffc70507f8bd036a4216
Casey 'Cubusky' Hofland <hofland.casey@gmail.com>
2026-08-15 17:47:19 +0200

    Add an empty test projects for BuildingBlocks and Stats.Core
    Leave Stats.Generators out of this for now because I need to research how to do proper test projects for generators again.

---
+++ Cubusky.BuildingBlocks.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Cubusky.BuildingBlocks.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ Cubusky.Stats.Core.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+﻿namespace Cubusky.Stats.Core.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
