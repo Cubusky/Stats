a3e1705adffb3c159b8edb8bed28dc36b3239b29
Casey 'Cubusky' Hofland <hofland.casey@gmail.com>
2026-08-16 13:49:53 +0200

    Remove BuildingBlocks from the Stats solution
    It is its own project now.

--- Cubusky.BuildingBlocks/IBroadcaster.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Cubusky.BuildingBlocks;
-
-public interface IBroadcaster<out TConformance>
-{
-    void Broadcast<TBroadcast>(in TBroadcast broadcast)
-        where TBroadcast : struct, IBroadcast<TConformance>;
-}
--- Cubusky.BuildingBlocks/IBuildingBlock.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace Cubusky.BuildingBlocks;
-
-public interface IOperation<TConformance>;
-public interface IBroadcast<in TConformance>;
-public interface IBuildingBlock<TConformance> : IOperation<TConformance>, IBroadcast<TConformance>;
